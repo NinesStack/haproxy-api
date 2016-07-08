dcf8b6b223cc7fe40d459110da7d11a5ede720eb
Karl Matthias <karl.matthias@gonitro.com>
2016-07-08 10:04:22 +0100

    Move toml file around.

--- docker/build.sh
+++ docker/build.sh
@@ -7,5 +7,5 @@ die() {
 
 file ../haproxy-api | grep "ELF.*LSB" || die "../haproxy-api is missing or not a Linux binary"
 
-test -f haproxy.toml || cp haproxy.docker.toml haproxy.toml
+test -f haproxy-api.toml || cp haproxy-api.docker.toml haproxy-api.toml
 cp ../haproxy-api . && cp -pr ../templates . && docker build -t haproxy-api . || die "Failed to build"
